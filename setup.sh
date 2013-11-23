f0b44bb14f54e9b24a576b8a10b63f4bda4a8e6c
robbinhan <luckyhanbin@gmail.com>
2013-11-23 18:35:44 +0800

    一键新系统需要安装的软件1.0

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+brew install wget
+brew install git
+brew cask install alfred
+brew cask install keka
+brew cask install qq
+brew cask install mplayerx
+brew cask install evernote
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install mou
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install phpstorm
+brew cask install skim
+brew cask install appzapper
+brew cask install flux
+brew cask install filezilla
+brew cask install rtx
+brew cask install sequel-pro
+brew cask install goagentx
+brew cask install xbmc
+brew cask install airmail-beta
+brew cask install tunnelblick
+brew cask install caffeine
+brew cask install keyremap4macbook
